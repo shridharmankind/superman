50814630e2f1cf6a756458565d4453be2bbd75fc
manoj.maurya <manoj.maurya@nagarro.com>
2021-05-13 00:30:12 +0530

    Configured directory aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _elements: './src/components/elements',
+          _widgets: './src/components/widgets',
+          _navigations: './src/navigations',
+          _generic: './src/screens/generic',
+          _sales: './src/screens/sales',
+          _tourPlan: './src/screens/tour-plan',
+          _services: './src/services',
+          _themes: './src/themes',
+          _utils: './src/utils',
+          _common: './src/common',
+          _database: './src/database',
+        },
+      },
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        root: ['./src'],
+        extensions: ['.js', '.ios.js', '.android.js'],
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _elements: './src/components/elements',
+          _widgets: './src/components/widgets',
+          _navigations: './src/navigations',
+          _generic: './src/screens/generic',
+          _sales: './src/screens/sales',
+          _tourPlan: './src/screens/tour-plan',
+          _services: './src/services',
+          _themes: './src/themes',
+          _utils: './src/utils',
+          _common: './src/common',
+          _database: './src/database',
+        },
+      },
+    ],
+  ],
 };
