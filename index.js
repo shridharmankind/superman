b3794c7790af674b9e3e3170e901514563e30538
Sakshi Aggarwal <sakshi.aggarwal@nagarro.com>
2021-05-13 19:36:32 +0530

    SUP-119: added button component

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import { Provider as PaperProvider } from 'react-native-paper';
-import theme from './src/themes';
+import theme from 'themes';
 
 export default function Main() {
     return (
