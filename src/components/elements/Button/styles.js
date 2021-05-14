89371f44d367f4c4b0380bde34c8d81c86e110ee
Sakshi Aggarwal <sakshi.aggarwal@nagarro.com>
2021-05-14 18:42:24 +0530

    SUP-340: use touchable opacity for button

--- src/components/elements/Button/styles.js
+++ src/components/elements/Button/styles.js
@@ -2,14 +2,30 @@ import {StyleSheet} from 'react-native';
 import theme from 'themes';
 
 const styles = StyleSheet.create({
-  appButtonContainer: {
+  buttonContainer: {
     borderRadius: theme.roundness,
     justifyContent: 'center',
     alignItems: 'center',
     height: 50,
   },
-  appButtonText: {
+  buttonText: {
     fontSize: 14,
+    fontWeight: 'bold',
+    color: theme.colors.white,
+  },
+  buttonContained: {
+    backgroundColor: theme.colors.primary,
+  },
+  buttonOutlined: {
+    backgroundColor: theme.colors.white,
+    borderColor: theme.colors.primary,
+    borderWidth: 2,
+  },
+  buttonOutlinedText: {
+    color: theme.colors.primary,
+  },
+  button: {
+    borderColor: 'transparent',
   },
   disabledButton: {
     backgroundColor: theme.colors.disabled,
