803c12ba6a57f67bad0b94df0f12643cfaeb443e
Puneet Nagpal <puneet.nagpal@nagarro.com>
2021-05-14 10:14:09 +0530

    mocking of data implementation

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import useMock from "./data/mock"; 
+import env from '../env.json'
+
+const mockRequests = true;
+const baseURL = env.API_HOST;
+
+const client = axios.create({
+  baseURL,
+});
+
+if (env.ENVIRONMENT === "STATIC" && env.MOCK_REQUESTS === 'ALL') {
+  useMock(client);
+}
+
+export const fetchAllUsers = () => {
+  return client.get("/all-users");
+};
+
+export const fetchSingleUser = (id) => {
+  return client.get(`/single-user?id=${id}`);
+};
--- src/screens/generic/Login/index.js
+++ src/screens/generic/Login/index.js
@@ -3,11 +3,16 @@ import {Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import styles from './styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { fetchAllUsers } from '../../../api'
 
 export default function Login({navigation}) {
   const {colors} = useTheme();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const getUserList = () => {
+    fetchAllUsers().then(res => console.log(res.data))
+  }
 
   return (
     <View style={styles.container}>
@@ -47,6 +52,12 @@ export default function Login({navigation}) {
         style={styles.loginBtn}
         onPress={() => navigation.navigate('Home')}>
         <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => getUserList()}>
+        <Text style={styles.loginText}>Get Users list</Text>
       </TouchableOpacity>
     </View>
   );
