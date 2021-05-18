1da2e71386dd8c0c547ed3a39087bcead821180b
Rishabh Bhatia <rishabh.bhatia@nagarro.com>
2021-05-18 23:06:53 +0400

    Dashboard reflect nav page changes on content :wip

--- src/screens/generic/Dashboard/index.js
+++ src/screens/generic/Dashboard/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
 
 import {NotificationIcon, SearchIcon} from 'assets';
 import NavMenu from './components/NavMenu';
+import {Label} from 'components/elements';
+import navMenuData from './components/NavMenu/navMenuData';
 
 import styles from './styles';
 
 const Dashboard = () => {
+  const [activePage, setActivePage] = useState(0);
+  const [isNavOpen, setIsNavOpen] = useState(true);
+
+  const onActivePageChanged = pageIndex => {
+    setActivePage(pageIndex);
+  };
+
+  useEffect(() => {
+    // transition of content layout to left side
+  }, [isNavOpen]);
+
+  const onNavToggled = open => {
+    setIsNavOpen(open);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.sidemenuContainer}>
-        <NavMenu />
+        <NavMenu
+          onNavItemPress={onActivePageChanged}
+          onNavToggled={onNavToggled}
+        />
       </View>
       <View style={styles.contentContainer}>
-        <View />
+        <View>
+          <Label title={navMenuData[activePage].label} />
+        </View>
       </View>
       <View style={styles.actionsContainer}>
         <View style={styles.action}>
