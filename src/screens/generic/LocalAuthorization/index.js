b012dc0227a863c2ec34800a73b394aeb610e3f4
Vinod Chauhan <vinod.chauhan@nagarro.com>
2021-05-24 20:11:31 +0530

    Changes for Auth

--- src/screens/generic/LocalAuthorization/index.js
+++ src/screens/generic/LocalAuthorization/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { View } from 'react-native';
-// import LocalAuthentication from 'rn-local-authentication';
-// import PasscodeAuth from 'react-native-passcode-auth';
-import DeviceInfo from 'react-native-device-info';
-import { Constants } from 'react-native-unimodules';
 import * as LocalAuthentication from 'expo-local-authentication';
 
 // ...
@@ -13,10 +9,9 @@ class LocalAuthorization extends React.Component {
   }
 
   componentDidMount() {
-    console.log('1',Constants.systemFonts);
+    
     LocalAuthentication.hasHardwareAsync()
     .then(response => {
-      console.log(response)
       LocalAuthentication.supportedAuthenticationTypesAsync()
       .then(response2 => {
         console.log("available",response2)
@@ -33,26 +28,7 @@ class LocalAuthorization extends React.Component {
       })
       
     })
-    // LocalAuthentication.authenticateAsync({
-    //     reason: "Very important reason to authenticate",
-    //     fallbackEnabled: true,
-    //     fallbackTitle: "Enter password",
-    //     cancelTitle: "Cancel",
-    //     reuseDuration: "300"
-    // }).then(response => {
-    //   if (response.success) {
-    //     console.log('Authorized successfully!');
-    //     this.props.navigation.navigate('Login');
-    //   } else {
-    //     console.log(DeviceInfo.isPinOrFingerprintSetSync());
-    //     console.log(DeviceInfo.isPinOrFingerprintSet());
-    //     console.log(DeviceInfo.getFingerprint());
-    //     console.log(DeviceInfo.getFingerprintSync());
-    //     console.log(DeviceInfo.getPhoneNumber());
-    //     console.log(`Something went wrong. Error: ${response.error}`);
-    //     //fetchPassword();
-    //   }
-    // })
+    
 
     fetchPassword = () => {
       PasscodeAuth.authenticate('to demo this react-native component')
