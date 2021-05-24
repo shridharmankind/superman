0e1e1856263b3763f51814dc1f7c9416b8158079
Vinod Chauhan <vinod.chauhan@nagarro.com>
2021-05-24 20:37:50 +0530

    Add Authorization

--- src/screens/generic/LocalAuthorization/index.js
+++ src/screens/generic/LocalAuthorization/index.js
@@ -1,51 +1,46 @@
 import React from 'react';
-import { View } from 'react-native';
+import {View} from 'react-native';
 import * as LocalAuthentication from 'expo-local-authentication';
 
 // ...
 class LocalAuthorization extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
   componentDidMount() {
-    
-    LocalAuthentication.hasHardwareAsync()
-    .then(response => {
-      LocalAuthentication.supportedAuthenticationTypesAsync()
-      .then(response2 => {
-        console.log("available",response2)
-      })
-      LocalAuthentication.getEnrolledLevelAsync()
-      .then(response2 => {
-        console.log("enrolled - ",response2);
+    LocalAuthentication.hasHardwareAsync().then(response => {
+      LocalAuthentication.supportedAuthenticationTypesAsync().then(
+        response2 => {
+          console.log('available', response2);
+        },
+      );
+      LocalAuthentication.getEnrolledLevelAsync().then(response2 => {
+        console.log('enrolled - ', response2);
 
-        LocalAuthentication.authenticateAsync()
-        .then(response3 => {
+        LocalAuthentication.authenticateAsync().then(response3 => {
           console.log(response3);
           this.props.navigation.navigate('Login');
-        })
-      })
-      
-    })
-    
+        });
+      });
+    });
 
     fetchPassword = () => {
       PasscodeAuth.authenticate('to demo this react-native component')
-      .then(success => {
-        // Success code
-        console.log('Authorized successfully!');
-        this.props.navigation.navigate('Login');
-      })
-      .catch(error => {
-        // Failure code
-        console.log(`Something went wrong. Error: ${error}`);
-      });
-    }
+        .then(success => {
+          // Success code
+          console.log('Authorized successfully!');
+          this.props.navigation.navigate('Login');
+        })
+        .catch(error => {
+          // Failure code
+          console.log(`Something went wrong. Error: ${error}`);
+        });
+    };
   }
 
   render() {
-    return (<View />);
+    return <View />;
   }
 
   // ...
