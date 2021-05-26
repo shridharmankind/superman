744056314b191a48f2ae95ef210addd44673529c
Sakshi Aggarwal <sakshi.aggarwal@nagarro.com>
2021-05-26 23:24:14 +0530

    cleanup

--- src/screens/tour-plan/monthly/redux/monthlySlice.js
+++ src/screens/tour-plan/monthly/redux/monthlySlice.js
@@ -1,6 +1,5 @@
 import {createAction, createSlice} from '@reduxjs/toolkit';
 import merge from 'lodash.merge';
-import {GET_SUBORDINATES} from 'screens/tourPlan/actionConstants';
 
 /**
  * Initial state
@@ -14,14 +13,14 @@ export const monthlyTourPlan = {
 /**
  *  redux-saga actions
  */
-export const getSubordinatesCreator = createAction(GET_SUBORDINATES);
+export const getSubordinatesCreator = createAction('GET_SUBORDINATES');
 export const getSubordinatesTypeName = getSubordinatesCreator().type;
 
 /**
  *  create subordinate slice defining the intial state, reducers
  */
 export const getSubordinateSlice = createSlice({
-  name: GET_SUBORDINATES,
+  name: 'GET_SUBORDINATES',
   initialState: monthlyTourPlan,
   reducers: {
     getSubordinates: (state, action) => merge(state, action.payload),
