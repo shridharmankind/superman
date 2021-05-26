d19c1c773b5f16441672e6a71651008d6e9725b5
Sakshi Aggarwal <sakshi.aggarwal@nagarro.com>
2021-05-26 23:19:28 +0530

    removed action constants

--- src/screens/tour-plan/daily/redux/dailySlice.js
+++ src/screens/tour-plan/daily/redux/dailySlice.js
@@ -1,6 +1,5 @@
 import {createAction, createSlice} from '@reduxjs/toolkit';
 import merge from 'lodash.merge';
-import {FETCH_DOCTOR_DETAIL} from 'screens/tourPlan/actionConstants';
 
 /**
  * Initial state of doctor details
@@ -14,14 +13,14 @@ export const doctorDetailState = {
 /**
  *  redux-saga actions
  */
-export const fetchDoctorDetailCreator = createAction(FETCH_DOCTOR_DETAIL);
+export const fetchDoctorDetailCreator = createAction('FETCH_DOCTOR_DETAIL');
 export const fetchDoctorDetailTypeName = fetchDoctorDetailCreator().type;
 
 /**
  *  create doctor detail slice defining the intial state, reducers
  */
 export const doctorDetailSlice = createSlice({
-  name: FETCH_DOCTOR_DETAIL,
+  name: 'FETCH_DOCTOR_DETAIL',
   initialState: doctorDetailState,
   reducers: {
     getDoctorDetail: (state, action) => merge(state, action.payload),
