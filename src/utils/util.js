88a9a55898cf21c70aee70ba2d88a20f6dcc502d
manoj.maurya <manoj.maurya@nagarro.com>
2021-05-27 18:01:13 +0530

    Review Comment

--- src/utils/util.js
+++ src/utils/util.js
@@ -5,11 +5,9 @@ export const isAccessTokenValid = async () => {
   const tokenExpiryTime = await AsyncStorage.getItem(
     Constants.TOKEN_EXPIRY_TIME,
   );
-  console.log('Manoj1', tokenExpiryTime);
   const currentTime = Date.now();
   const diff = currentTime - tokenExpiryTime * 1000;
   if (diff <= 0) {
-    return 11;
+    return true;
   }
-  return 10;
 };
