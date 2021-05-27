87650a33ccf250fea3e9d24fad009067f488d60a
Manpreet <manpreet.kaur02@nagarro.com>
2021-05-27 14:22:07 +0530

    Update apiPath.js

--- src/screens/tour-plan/apiPath.js
+++ src/screens/tour-plan/apiPath.js
@@ -1,7 +1,3 @@
-export const getParties = 'Mtp/getParties';
-export const getSubordinates = 'staff/getsubordinates';
-export const workingDay = '/stp/workingday';
-
 export const API_PATH = {
   GET_PARTIES: 'Mtp/getParties',
   GET_SUBORDINATES: 'staff/getsubordinates',
