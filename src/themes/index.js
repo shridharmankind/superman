1c4180d6a23cad6cdf71d94aa0da3f60cfb126ba
Rishabh Bhatia <rishabh.bhatia@nagarro.com>
2021-05-27 13:45:14 +0400

    Typography extended with style guide, color palette and typography specifics moved into individual files for cleaner theme code :wip

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,48 +1,27 @@
 import {DefaultTheme} from 'react-native-paper';
 
+import Colors from './colors';
+import Typography, {fontFamilies} from './typography';
+
 const theme = {
   ...DefaultTheme,
+  spacing: space => {
+    /**
+     * manage size ratio using the spacing factor
+     * ex 8/1.5 scales down everything by 1.5 times
+     */
+    const spacingFactor = 8;
+    return spacingFactor * space;
+  },
+  typography: {
+    ...Typography,
+  },
   colors: {
     ...DefaultTheme.colors,
-    background: '#F5F8F9',
-    accent: '#ff7171',
-    black: '#000',
-    disabled: '#E4E4E4',
-    grayishBlue: '#D5E2E7',
-    lightGrayishBlue: '#F5F8F9',
-    primary: '#322b7c',
-    veryPaleRed: '#FFC0CB',
-    white: '#fff',
-    orange: '#FFA088',
-    borderColor: '#979797',
-    darkBlue: '#5920B7',
-    lightBlue: '#0852C5',
-    limeGreen: '#00B637',
-    checkCircleBlue: '#0095d1',
-    transparent: '#00000000',
-    red: {
-      100: '#E36565',
-      200: '#f45b60',
-    },
-    blueBg: '#50478D',
-    softRed: '#E36565',
-    grey: {
-      100: '#BEBEBF',
-      200: '#1C1939',
-      300: '#e3e3e3',
-      400: '#e2e2e2',
-      500: '#6D7278',
-      600: '#464A53',
-    },
-    blueShades: {100: '#32C5FF14'},
-    pink: {100: '#FF71E7'},
+    ...Colors,
   },
   fonts: {
-    fontRegular: 'Poppins-Regular',
-    fontBold: 'Poppins-Bold',
-    fontLight: 'Poppins-Light',
-    fontSemiBold: 'Poppins-SemiBold',
-    fontItalic: 'Poppins-Italic',
+    ...fontFamilies,
   },
   roundness: 10,
 };
---
+++ src/themes/typography.js
@@ -0,0 +1,36 @@
+export const fontFamilies = {
+  fontRegular: 'Poppins-Regular',
+  fontBold: 'Poppins-Bold',
+  fontLight: 'Poppins-Light',
+  fontSemiBold: 'Poppins-SemiBold',
+  fontItalic: 'Poppins-Italic',
+};
+
+const Typography = {
+  h1: {
+    fontSize: 48,
+    fontFamily: fontFamilies.fontRegular,
+  },
+  h2: {
+    fontSize: 34,
+    fontFamily: fontFamilies.fontRegular, // Poppins-Medium
+  },
+  h3: {
+    fontSize: 28,
+    fontFamily: fontFamilies.fontSemiBold,
+  },
+  h4: {
+    fontSize: 21,
+    fontFamily: fontFamilies.fontSemiBold,
+  },
+  h5: {
+    fontSize: 19,
+    fontFamily: fontFamilies.fontSemiBold,
+  },
+  subtitleLarge: {
+    fontSize: 24,
+    fontFamily: fontFamilies.fontRegular,
+  },
+};
+
+export default Typography;
