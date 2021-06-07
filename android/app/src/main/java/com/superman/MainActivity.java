c88004062240f04e39a3a8b79166a147b69e4dc4
Shiv Yadav <shiv.yadav01@nagarro.com>
2021-06-08 00:32:24 +0530

    added full splash screen

--- android/app/src/main/java/com/superman/MainActivity.java
+++ android/app/src/main/java/com/superman/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
   }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this, true); // <- second parameter is true, to hide StatusBar
+    SplashScreen.show(this, R.style.SplashTheme); // <- second parameter is true, to hide StatusBar
     super.onCreate(savedInstanceState);
     hideNavigationBar();
   }
