78eb6ca343e0f2a9170d160ab4b103d4533c9df2
Manpreet <manpreet.kaur02@nagarro.com>
2021-06-10 20:38:07 +0530

    DOctor Tag comp Created

---
+++ src/components/widgets/DoctorTag/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+import themes from 'themes';
+
+const styles = StyleSheet.create({
+  container: {
+    borderTopLeftRadius: 7,
+    borderBottomRightRadius: 7,
+    paddingVertical: themes.spacing(4),
+    width: 37.3,
+  },
+  labelContent: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+});
+
+export default styles;
