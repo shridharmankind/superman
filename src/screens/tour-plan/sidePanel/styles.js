d9ed38f9c4ac7e9c69447dce74cab53a2303cb43
Manpreet <manpreet.kaur02@nagarro.com>
2021-06-17 16:03:37 +0530

    Update styles.js
    style fixes

--- src/screens/tour-plan/sidePanel/styles.js
+++ src/screens/tour-plan/sidePanel/styles.js
@@ -3,7 +3,7 @@ import theme from 'themes';
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: theme.spacing(21.3),
+    marginBottom: theme.spacing(21.3),
     paddingHorizontal: theme.spacing(2),
   },
 });
