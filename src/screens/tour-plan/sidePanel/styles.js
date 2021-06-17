a308c55e5657bbe8a258b4aad00f9efa0a89be52
Manpreet <manpreet.kaur02@nagarro.com>
2021-06-17 11:10:48 +0530

    Tour plan Side Panel

---
+++ src/screens/tour-plan/sidePanel/styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import theme from 'themes';
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: theme.spacing(21.3),
+    paddingHorizontal: theme.spacing(2),
+  },
+});
+
+export default styles;
