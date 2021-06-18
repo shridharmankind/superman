0c2bb226c6004c4eb0880311caa003f97ca82481
Shiv Yadav <shiv.yadav01@nagarro.com>
2021-06-19 03:38:39 +0530

    added biometrics

--- src/screens/generic/LocalAuthentication/index.js
+++ src/screens/generic/LocalAuthentication/index.js
@@ -1,45 +1,79 @@
 import React, {useRef, useState, useEffect} from 'react';
-import {AppState, StyleSheet, Text, View} from 'react-native';
+import {AppState, StyleSheet, Text, View, Alert} from 'react-native';
 import * as LocalAuthentication from 'expo-local-authentication';
 import AsyncStorage from '@react-native-community/async-storage';
 
 const LocalAuth = ({navigation}) => {
+  const [isBiometricSupported, setIsBiometricSupported] = useState(false);
+  const appState = useRef(AppState.currentState);
   useEffect(() => {
-    // AppState.addEventListener('change', _handleAppStateChange);
+    (async () => {
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      setIsBiometricSupported(compatible);
+    })();
+    AppState.addEventListener('change', _handleAppStateChange);
     checkForIsloggedIn();
 
     return () => {
       AsyncStorage.removeItem('isLoggedIn');
-      // AppState.removeEventListener('change', _handleAppStateChange);
+      AppState.removeEventListener('change', _handleAppStateChange);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  const fallBackToDefaultAuth = async () => {
+    const response = await LocalAuthentication.authenticateAsync();
+    if (response.success) {
+      await AsyncStorage.setItem('isLoggedIn', 'true');
+      navigation.navigate('Login');
+      return '';
+    } else {
+      checkForIsloggedIn();
+    }
+  };
 
-  // const _handleAppStateChange = async nextAppState => {
-  //   if (
-  //     appState.current.match(/inactive|background/) &&
-  //     nextAppState === 'active'
-  //   ) {
-  //     console.log('App has come to the foreground!');
-  //     const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
-  //     console.log('');
-  //     if (isLoggedIn === null) {
-  //       checkForIsloggedIn();
-  //     } else {
-  //       navigation.navigate('Login');
-  //     }
-  //   }
+  const alertComponent = (title, mess, btnTxt, btnFunc) => {
+    return Alert.alert(title, mess, [
+      {
+        text: btnTxt,
+        onPress: btnFunc,
+      },
+    ]);
+  };
+  const _handleAppStateChange = async nextAppState => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      console.log('App has come to the foreground!');
+      const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+      if (JSON.parse(isLoggedIn)) {
+        navigation.navigate('Login');
+      }
+    }
 
-  //   appState.current = nextAppState;
-  //   setAppStateVisible(appState.current);
-  //   console.log('AppState', appState.current);
-  // };
+    appState.current = nextAppState;
+    console.log('AppState', appState.current);
+  };
 
   const checkForIsloggedIn = async () => {
-    const response = await LocalAuthentication.authenticateAsync();
+    // Check if hardware supports biometrics
+    const isBiometricAvailable = await LocalAuthentication.hasHardwareAsync();
+    // Fallback to default authentication method (password) if Fingerprint is not available
+    if (!isBiometricAvailable)
+      return alertComponent(
+        'Please enter your password',
+        'Biometric Authentication not supported',
+        'OK',
+        () => fallBackToDefaultAuth(),
+      );
+    const response = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login with Biometrics',
+      cancelLabel: 'Cancel',
+      disableDeviceFallback: false,
+    });
     console.log(response, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
     if (response.success) {
-      AsyncStorage.setItem('isLoggedIn', 'true');
+      await AsyncStorage.setItem('isLoggedIn', 'true');
       navigation.navigate('Login');
       return '';
     } else {
