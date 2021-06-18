b8952323110bb5201e261da09e01e79dc2c0569f
Rishabh Bhatia <rishabh.bhatia@nagarro.com>
2021-06-18 12:27:57 +0400

    Qualifications helper operations added

--- src/database/operations/qualificationOperations.js
+++ src/database/operations/qualificationOperations.js
@@ -22,9 +22,32 @@ export default dbInstance => ({
     return recordsUpdated;
   },
   getQualification: async id => {
-    return await dbInstance.findOne({id});
+    return await dbInstance.objectForPrimaryKey(QualificationsSchemaName, id);
   },
   getQualifications: async () => {
     return await getAllTableRecords(QualificationsSchemaName);
   },
+  getQualificationsByDivision: async divisionId => {
+    const qualifications = await getAllTableRecords(QualificationsSchemaName);
+    return await qualifications.filtered(`divisionId = ${divisionId}`);
+  },
 });
+
+/** Sample usage */
+/* const loadQualifications = async () => {
+  const qualifications = await Qualifications.getQualifications();
+  const qualification = await Qualifications.getQualification(2);
+  const qualificationsByDivision =
+    await Qualifications.getQualificationsByDivision(2);
+
+  qualifications.forEach(qualificationItem => {
+    console.log('DB qualifications', qualificationItem.name);
+  });
+
+  console.log('DB qualification by ID', qualification.name);
+
+  qualificationsByDivision.forEach(qualificationItem => {
+    console.log('DB qualifications by division ID', qualificationItem.name);
+  });
+};
+loadQualifications(); */
