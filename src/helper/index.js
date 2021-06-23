6cededef29f83f476c205134dcca259e200edeac
Manpreet <manpreet.kaur02@nagarro.com>
2021-06-23 21:34:39 +0530

    Update index.js

--- src/helper/index.js
+++ src/helper/index.js
@@ -2,7 +2,8 @@ import {Platform} from 'react-native';
 
 import * as KeyChain from './keychain';
 import CircularProgressBarWithStatus from './progressBar';
+import * as CustomHook from './customHook';
 
 export const isWeb = () => Platform.OS === 'web';
 
-export {KeyChain, CircularProgressBarWithStatus};
+export {KeyChain, CircularProgressBarWithStatus, CustomHook};
