f1bd5849d39182522fc2c0cd8cefacaa6d2c1f78
Manpreet <manpreet.kaur02@nagarro.com>
2021-06-25 21:01:39 +0530

    Update helper.js

--- src/screens/tour-plan/helper.js
+++ src/screens/tour-plan/helper.js
@@ -121,7 +121,7 @@ export const getSelectedPartyTypeData = (
   if (!updatedPatchArray || !updatedPatchArray.length) {
     return;
   }
-
+console.log('XMonthValue',XMonthValue,'selectedDayNumber',selectedDayNumber)
   const isDayWithinXDays = selectedDayNumber <= XMonthValue?.xValue;
 
   updatedPatchArray.map(party => {
