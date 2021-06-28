20a95f991789f345330780ec71cf506bd1dbb539
Rishabh Bhatia <rishabh.bhatia@nagarro.com>
2021-06-28 17:11:51 +0400

    Web nested linking url support added, nginx conf file and docker ignore files added

--- src/navigations/linking.js
+++ src/navigations/linking.js
@@ -1,29 +1,75 @@
-import Routes from './routes';
-import HomeRoutes, {ROUTES_HOME} from 'screens/home/routes';
-import ROUTES_DASHBOARD from 'screens/generic/Dashboard/routes';
-
+/** docs
+ * https://reactnavigation.org/blog/2020/05/16/web-support/
+ * https://reactnavigation.org/docs/configuring-links/#playground
+ * */
 const origin = (window && window.location && window.location.origin) || '';
 const prefixes = [origin, 'com.superman://'];
-
-const getScreensConfigFromRoutes = routes => {
-  const screens = {};
-
-  routes.forEach(route => {
-    const path = route.path || route.name.toLowerCase();
-    screens[Object.values(route)[0]] = path;
-  });
-
-  return screens;
-};
 
 const linking = {
   prefixes,
   config: {
     screens: {
-      ...getScreensConfigFromRoutes(Routes),
-      ...getScreensConfigFromRoutes(ROUTES_DASHBOARD),
-      ...getScreensConfigFromRoutes(HomeRoutes),
-      ...getScreensConfigFromRoutes(ROUTES_HOME),
+      Login: 'login',
+      Auth: 'auth',
+      MasterDataDownload: 'master-data-download',
+      Dashboard: {
+        path: '',
+        screens: {
+          Home: {
+            path: 'home',
+            screens: {
+              HomeLanding: 'landing',
+            },
+          },
+          TourPlan: 'tour-plan',
+          Performance: {
+            path: 'performance',
+            screens: {
+              PerformanceLanding: 'landing',
+            },
+          },
+          Directory: {
+            path: 'directory',
+            screens: {
+              DirectoryLanding: 'landing',
+              DoctorProfile: 'profile',
+              EDetailing: 'e-detailing',
+            },
+          },
+          GSP: {
+            path: 'gsp',
+            screens: {
+              PerformanceLanding: 'landing',
+            },
+          },
+          Admin: {
+            path: 'admin',
+            screens: {
+              PerformanceLanding: 'landing',
+            },
+          },
+          Content: {
+            path: 'content',
+            screens: {
+              PerformanceLanding: 'landing',
+            },
+          },
+          Learn: {
+            path: 'learn',
+            screens: {
+              PerformanceLanding: 'landing',
+            },
+          },
+          Settings: {
+            path: 'settings',
+            screens: {
+              PerformanceLanding: 'landing',
+            },
+          },
+        },
+      },
+      StandardPlan: 'standard-plan',
+      DoctorFeedback: 'doctor-feedback',
     },
   },
 };
--- src/screens/performance/routes.js
+++ src/screens/performance/routes.js
@@ -1,1 +1,11 @@
+import PerformanceLanding from './landing';
+
 export const ROUTE_PERFORMANCE_LANDING = 'PerformanceLanding';
+
+export const ROUTES_PERFORMANCE = [
+  {
+    name: ROUTE_PERFORMANCE_LANDING,
+    component: PerformanceLanding,
+    path: '/landing',
+  },
+];
