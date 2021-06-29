b3d86146870ceab74f5f3a79ac8da3bc8b9898e9
Manpreet <manpreet.kaur02@nagarro.com>
2021-06-29 10:47:10 +0530

    Refactor Selector

--- src/store/selectors/appSelector.js
+++ src/store/selectors/appSelector.js
@@ -1,14 +1,22 @@
 import {createSelector} from '@reduxjs/toolkit';
 
-const getFetch = state => state.appState;
-
+const getFetch = state => state.appState.appStatus;
 const getFetchState = createSelector([getFetch], fetch =>
   fetch === {} ? 'FETCHING' : fetch,
+);
+
+const getAppData = state => state.appState;
+const staffPositionIdSelector = createSelector(
+  [getAppData],
+  data => data?.staffPositionId,
 );
 
 export const appSelector = {
   //App state
   makeGetAppFetch: () => {
     return getFetchState;
+  },
+  getStaffPositionId: () => {
+    return staffPositionIdSelector;
   },
 };
