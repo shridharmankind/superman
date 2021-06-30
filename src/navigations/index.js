fa6acfed4d02c5534c99e86b9688f0c81017b825
Rishabh Bhatia <rishabh.bhatia@nagarro.com>
2021-06-30 14:53:29 +0400

    navigation service removed

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,5 +1,4 @@
 import * as Routes from './routes';
 import linking from './linking';
-import * as NavigationService from './NavigationService';
 
-export {Routes, linking, NavigationService};
+export {Routes, linking};
