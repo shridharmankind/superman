042f1c6e59b582a9381275f0c5f098abb3a076a7
Anirudh Chauhan <anirudh.chauhan@nagarro.com>
2021-07-01 20:48:04 +0530

    updating name

--- src/database/schemas/MotherbrandType.js
+++
@@ -1,8 +0,0 @@
-export const MotherBrandSchemaName = 'MotherBrandType';
-export default class MotherBrandTypeSchema {
-  static schema = {
-    name: MotherBrandSchemaName,
-    primaryKey: 'id',
-    properties: {id: 'int', name: 'string?', shortName: 'string?'},
-  };
-}
